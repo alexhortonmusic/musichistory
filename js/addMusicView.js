e816e474db155022bffd89ac5240aa2b87978c5c
Alex Horton <alexhortonmusic@gmail.com>
2016-06-28 13:23:35 -0500

    list view/add view works for version-4

--- js/addMusicView.js
+++ js/addMusicView.js
@@ -1,34 +1,17 @@
-var secondLevel = document.getElementById('second-level'); // container
-var addMusicView = document.getElementById('addMusicView');// add music link
+$(document).ready(function () {
 
-var listMusicView = document.getElementById('listMusicView'); // list music link
-var listView = document.getElementById('listView'); // container holding list view divs
-var addMusic = document.getElementById('addMusic'); // div added on 'add music' click event
+	var secondLevel = document.getElementById('second-level'); // container
 
-
-addMusicView.addEventListener('click', function () {
-	listView.classList.toggle('hide');
-	addMusicDiv();
-	
-});
+	var addMusicView = document.getElementById('addMusicView');// add music link
+	var listMusicView = document.getElementById('listMusicView'); // list music link
+	var listView = document.getElementById('listView'); // container holding list view divs
+	var addMusic = document.getElementById('addMusic'); // div added on 'add music' click event
 
 
-function addMusicDiv () {
-	secondLevel.innerHTML = `
-		<div class="addMusic" id="addMusic">
-			<label>Song name:</label>
-			<input type="text" id="songInput" class="musicInput">
-			<label>Artist name:</label>
-			<input type="text" id="artistInput" class="musicInput">
-			<label>Album name:</label>
-			<input type="text" id="albumInput" class="musicInput">
-			<label>Genre</label>
-			<input type="text" id="genreInput" class="musicInput">
-			<button id="addBtn">Add</button>
-		</div>
-	`;
-}
+	addMusicView.addEventListener('click', function () {
+		addMusic.classList.remove('hideAddView');
+		listView.classList.add('hideListView');
+	});
 
-listMusicView.addEventListener('click', function () {
-	// listView.classList.remove('hideListView');
-});
+
+})
